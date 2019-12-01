3a7144a4c13874dc09e19b815d1761b4b20fac77
Kevin <Kevin@TheNettles.com>
2019-11-30 18:12:01 -0800

    parsed json

--- boss_tsl.py
+++ boss_tsl.py
@@ -1,11 +1,22 @@
 import argparse
+import json
+
+
+def pretty(filename):
+    print('Filename: {}'.format(filename))
+
+    with open(filename, 'r') as file:
+        # text = file.read()
+        parsed = json.load(file)
+        print(json.dumps(parsed, indent=4))
 
 
 def main():
     parser = argparse.ArgumentParser(description='Print BOSS Tone Studio livesets.')
     parser.add_argument('filename', help="File name (*.tsl")
     args = parser.parse_args()
-    print('Filename: {}'.format(args.filename))
+
+    pretty(args.filename)
 
 if __name__ == '__main__':
     main()
