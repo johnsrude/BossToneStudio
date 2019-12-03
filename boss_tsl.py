af7527f48582f05a9e1efb8e105f2277baa3466b
Kevin <Kevin@TheNettles.com>
2019-12-02 23:06:08 -0800

    pedal fx

--- boss_tsl.py
+++ boss_tsl.py
@@ -3,6 +3,7 @@ import json
 import yaml
 
 
+
 def pretty(args):
     with open(args.filename, "r") as file:
         # text = file.read()
@@ -18,31 +19,48 @@ def pretty(args):
             print(f"{indent*1}{i % 4 + 1}: {patch['name']} ({i:02d})")
 
             if not args.patch_list:
-                # print(json.dumps(patch["params"], indent=4))
-                params = patch["params"]
-                pad = indent * 2
-                chain = "IN -> "
-                if params["comp_sw"] != '0': chain += f"COMP/FX1 -> "
-                if params["odds_sw"] != '0': chain += f"OD/DS -> "
-                if params["mod_sw"] != '0': chain += f"MOD -> "
-                if params["mod_sw"] != '0': chain += f"PREAMP -> "
-                if params["pdlfx_sw"] != '0': chain += f"PEDAL FX -> "
-                if params["fx2_sw"] != '0': chain += f"EQ/FX2 -> "
-                if params["dly_sw"] != '0': chain += f"DELAY -> "
-                if params["rev_sw"] != '0': chain += f"REVERB -> "
-                chain += f"OUT"
-
-                noise = int(patch['params']['ns_thresh'])
-                if noise > 0:
-                    chain += f"{pad}Noise: {noise}"
-
-                print(f"{pad}{chain}")
-
-
-                print(yaml.dump(patch["params"], allow_unicode=True, default_flow_style=False))
-                print("")
+                # print(json.dumps(params, indent=4))
+                print_patch(indent, patch)
         # if not args.patch_list:
         #     print(json.dumps(liveset, indent=4))
+
+pedal_fx_dict = {
+    "0" : "WAH",
+    "1" : "VOICE",
+    "2" : "+1 OCT",
+    "3" : "+2 OCT",
+    "4" : "-1 OCT",
+    "5" : "FREEZE",
+    "6" : "OSC DELAY",
+    "7" : "OD/DS",
+    "8" : "MOD RATE",
+    "9" : "DELAY LEV",
+}
+
+def print_patch(indent, patch):
+    params = patch["params"]
+    pad = indent * 2
+
+    pedal_fx = params["pdlfx_sw"] != '0'
+    pedal_fx_type = pedal_fx_dict[params['pdlfx_type']]
+    noise = int(params['ns_thresh'])
+
+    chain = "IN -> "
+    if pedal_fx: chain += f"PEDAL FX ({pedal_fx_type}) -> "
+    if params["comp_sw"] != '0': chain += f"COMP/FX1 -> "
+    if params["odds_sw"] != '0': chain += f"OD/DS -> "
+    if params["mod_sw"] != '0': chain += f"PREAMP -> "
+    if noise > 0: chain += f"NS ({noise}) -> "
+    if params["pdlfx_sw"] == '0': chain += f"PEDAL VOL -> "
+    if params["mod_sw"] != '0': chain += f"MOD -> "
+    if params["fx2_sw"] != '0': chain += f"EQ/FX2 -> "
+    if params["dly_sw"] != '0': chain += f"DELAY -> "
+    if params["rev_sw"] != '0': chain += f"REVERB -> "
+    chain += f"OUT"
+    print(f"{pad}{chain}")
+    print(f"CTL: TBD")
+    print(yaml.dump(params, allow_unicode=True, default_flow_style=False))
+    print("")
 
 
 def main():
